93773072b61bf2731061192a6df64d2e7b2d836e
Minahm Kim <mdkim105@gmail.com>
2016-06-10 10:56:37 -0400

    added home files

---
+++ app/components/home/homeController.js
@@ -0,0 +1,6 @@
+angular.module('ngPortfolio').controller('HomeController', function($scope){
+  // $scope.name = "Hello My Name is WHAT";
+}).component('home',{
+  controller: function(){},
+  templateUrl: 'app/components/home/home.html'
+});
